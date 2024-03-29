abeec49fe103cc67f24bb342cc1b5e5f55904340
David Talevski <davidtalevski00@gmail.com>
2024-03-29 15:59:56 +0100

    Fix before_plugin_install.js script

--- scripts/ios/before_plugin_install.js
+++ scripts/ios/before_plugin_install.js
@@ -8,7 +8,7 @@ var pluginVariables = {};
 
 var minCocoapodsVersion = "^1.11.2";
 
-module.exports = function(context) {
+module.exports = function (context) {
     checkCocoapodsVersion();
     removePodsLockFile(context);
 
@@ -19,29 +19,29 @@ module.exports = function(context) {
 
     pluginVariables = parsePluginVariables();
 
-    if(platforms.includes('ios') && pluginVariables.IOS_USE_PRECOMPILED_FIRESTORE_POD==='true'){
-        const pluginJSON=parsePluginXml();
+    if (platforms.includes('ios') && pluginVariables.IOS_USE_PRECOMPILED_FIRESTORE_POD === 'true') {
+        const pluginJSON = parsePluginXml();
         const iosPlatform = pluginJSON.plugin.platform.find((platform) => platform._attributes.name === 'ios');
         const firestorePod = iosPlatform.podspec.pods.pod.find((pod) => pod._attributes.name === 'FirebaseFirestore');
         firestorePod._attributes.tag = firestorePod._attributes.spec;
         firestorePod._attributes.git = 'https://github.com/invertase/firestore-ios-sdk-frameworks.git';
         delete firestorePod._attributes.spec;
-        writeJsonToXmlFile(pluginJSON, path.resolve("plugins/"+getPluginId()+"/plugin.xml"))
+        writeJsonToXmlFile(pluginJSON, path.resolve("plugins/" + getPluginId() + "/plugin.xml"))
     }
 };
 
-function checkCocoapodsVersion(){
+function checkCocoapodsVersion() {
     var version;
-    try{
-        version = execSync('pod --version', {encoding: 'utf8'}).match(/(\d+\.\d+\.\d+)/)[1];
-    }catch(err){
-        throw new Error("cocoapods not found - please install cocoapods >="+minCocoapodsVersion);
+    try {
+        version = execSync('pod --version', { encoding: 'utf8' }).match(/(\d+\.\d+\.\d+)/)[1];
+    } catch (err) {
+        throw new Error("cocoapods not found - please install cocoapods >=" + minCocoapodsVersion);
     }
 
-    if(!semver.valid(version)){
-        throw new Error("cocoapods version is invalid - please reinstall cocoapods@"+minCocoapodsVersion + ": "+version);
-    }else if(!semver.satisfies(version, minCocoapodsVersion)){
-        throw new Error("cocoapods version is out-of-date - please update to cocoapods@"+minCocoapodsVersion + " - current version: "+version);
+    if (!semver.valid(version)) {
+        throw new Error("cocoapods version is invalid - please reinstall cocoapods@" + minCocoapodsVersion + ": " + version);
+    } else if (!semver.satisfies(version, minCocoapodsVersion)) {
+        throw new Error("cocoapods version is out-of-date - please update to cocoapods@" + minCocoapodsVersion + " - current version: " + version);
     }
 }
 
@@ -51,6 +51,4 @@ function removePodsLockFile(ctx) {
         console.log("Removing Podfile.lock before plugin installation...");
         fs.unlinkSync(p);
     }
-}
-
 }
